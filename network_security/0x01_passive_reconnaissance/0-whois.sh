4a4a27abf157ad4461a294908e6884c21b6b5c77
AfsanaJabiyeva <cebiyevaefsane737@gmail.com>
2025-08-13 09:57:43 +0000

    fgh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 whois $1 | awk '/Registrant Organization|Registrant State\/Province|Registrant Country|Registrant Email|Admin Organization|Admin State\/Province|Admin Country|Admin Email|Tech Organization|Tech State\/Province|Tech Country|Tech Email/ { sub(": ", ","); print }' > $1.csv
+
