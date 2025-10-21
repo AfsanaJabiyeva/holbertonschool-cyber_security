638e0a24eebc138a5542f2287ffab6105967a64a
AfsanaJabiyeva <cebiyevaefsane737@gmail.com>
2025-10-21 08:26:51 +0000

    hash

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-if [ -z "$1" ]; then  echo "Usage: $0 <password>"  exit 1 fi
+[ -z "$1" ] && { echo "Usage: $0 <password>"; exit 1; }
 echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
